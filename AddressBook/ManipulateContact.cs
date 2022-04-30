a119e65b1ffba752e8ccadd6e1899402b381d04d
suraj <soorajkrpandit@gmail.com>
2022-04-30 14:58:28 +0530

    address conatct is added

---
+++ AddressBook/ManipulateContact.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    internal class ManipulateContact
+    {
+        List<AddressContact> AddressBookList = new List<AddressContact>();
+        public void AddingContact
+            (
+            string Name,
+            string Address,
+            string City,
+            string State,
+            string ZipCode,
+            string PhoneNumber,
+            string Email
+            )
+        {
+            AddressBookList.Add
+                (new AddressContact()
+                {
+                    Name = Name,
+                    Address = Address,
+                    City = City,
+                    State = State,
+                    ZipCode = ZipCode,
+                    PhoneNumber = PhoneNumber,
+                    Email = Email
+                }
+            );
+            Console.WriteLine($"{Name} is added___________");
+        }
+    }
+}
---
+++ AddressBook/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("welcome to address book system");
+            Console.WriteLine("how many contacts do you want to add?");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= num; i++)
+            {
+                AddressContact detail = new AddressContact();
+                ManipulateContact AddContact = new ManipulateContact();
+                AddContact.AddingContact(
+                    detail.Name = Console.ReadLine(),
+                    detail.Address = Console.ReadLine(),
+                    detail.City = Console.ReadLine(),
+                    detail.State = Console.ReadLine(),
+                    detail.ZipCode = Console.ReadLine(),
+                    detail.PhoneNumber = Console.ReadLine(),
+                    detail.Email = Console.ReadLine()
+                    );
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
+    
+
